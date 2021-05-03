859c7d71f0edda4828b1c422cebc7fe50c1bb7f0
Riccardo Fiorenza <ricfio.professional@gmail.com>
2021-05-03 02:25:31 +0200

    Update en.js for typos
    various typos

--- src/js/locale/en.js
+++ src/js/locale/en.js
@@ -2,10 +2,10 @@ export default {
   pageTitle: 'Times tables',
   metaDescription: 'Learn times tables with a quiz game',
   endMessage: 'Excellent! You completed all time tables with the score %d',
-  whatsYourName: "What's tyour name?",
-  whichMultiTable: 'Wich times table do you want repeat?',
+  whatsYourName: "What's your name?",
+  whichMultiTable: 'Which times table do you want repeat?',
   letsStart: "Let's go!",
-  noticeSelectAtLeastOne: 'Choose ate leats one times table to repeat',
+  noticeSelectAtLeastOne: 'Choose at least one times table to repeat',
   noticeInsertYourName: 'Hey! Insert your name :)',
   howMuchIs: 'How much Is',
 }
